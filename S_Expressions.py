4576d4b51d4c0e539767ea037ff8f685a9d8186e
Gianluca Pericoli <gianluca.pericoli@INTERLOGICA.LAN>
2024-03-10 17:53:35 +0100

    base commands

--- S_Expressions.py
+++ S_Expressions.py
@@ -1,16 +1,23 @@
 class Atom:
     def __init__(self, value):
         self.value = value
+
+    def is_numeric(self):
+        return self.value.isnumeric()
     
     def __repr__(self):
-        return repr(self.value)
+        return repr(f"Atom({self.value})")
     
-    def execute(self):
-        return self.value
 
-class S_List(list):
+class S_List():
+    def __init__(self, items):
+        self.items = items
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.items})"
+
     def execute(self):
-        return S_List(item.execute() if hasattr(item, 'execute') else item for item in self)
+        return S_List([item.execute() if hasattr(item, 'execute') else item for item in self.items])
 
 class ListOperation:
     def __init__(self, value):
@@ -22,7 +29,7 @@ class ListOperation:
     def execute(self):
         executed_value = self.value.execute() if hasattr(self.value, 'execute') else self.value
         
-        if not isinstance(executed_value, list):
+        if not isinstance(executed_value, S_List):
             print(f"Error: {self.__class__.__name__}: argument must be a list")
             exit(1)
 
@@ -30,8 +37,79 @@ class ListOperation:
 
 class CAR(ListOperation):
     def operation(self, executed_value):
-        return executed_value[0]
+        return executed_value.items[0]
 
 class CDR(ListOperation):
     def operation(self, executed_value):
-        return executed_value[1:]
+        return S_List(executed_value.items[1:])
+    
+class CONS(ListOperation):
+    def __init__(self, value1, value2):
+        self.value1 = value1
+        self.value2 = value2
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.value1}, {self.value2})"
+
+    def execute(self):
+        executed_value1 = self.value1.execute() if hasattr(self.value1, 'execute') else self.value1
+        executed_value2 = self.value2.execute() if hasattr(self.value2, 'execute') else self.value2
+        
+        if not isinstance(executed_value2, S_List):
+            print(f"Error: {self.__class__.__name__}: secondo argomento deve essere una lista")
+            exit(1)
+       
+        new_items = [executed_value1] + executed_value2.items
+        
+        return S_List(new_items)
+    
+class NULL(ListOperation):
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return f"NULL({self.value})"
+
+    def execute(self):
+        executed_value = self.value.execute() if hasattr(self.value, 'execute') else self.value
+
+        if not isinstance(executed_value, S_List):
+            print("Error: NULL: argument must be a list")
+            exit(1)
+
+        return len(executed_value.items) == 0
+
+
+class IS_ATOM:
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return f"IS_ATOM({self.value})"
+
+    def execute(self):
+        executed_value = self.value.execute() if hasattr(self.value, 'execute') else self.value
+
+        return isinstance(executed_value, Atom)
+    
+class EQ:
+    def __init__(self, value1, value2):
+        self.value1 = value1
+        self.value2 = value2
+
+    def __repr__(self):
+        return f"EQ({self.value1}, {self.value2})"
+
+    def execute(self):
+        executed_value1 = self.value1.execute() if hasattr(self.value1, 'execute') else self.value1
+        executed_value2 = self.value2.execute() if hasattr(self.value2, 'execute') else self.value2
+
+        if not isinstance(executed_value1, Atom) or not isinstance(executed_value2, Atom):
+            print("Error: two parameters must be atoms")
+            exit(1)
+        
+        # one or more atoms are numeric
+        if executed_value1.is_numeric() or executed_value2.is_numeric():
+            return False
+
+        return executed_value1.value == executed_value2.value
--- schema.py
+++ schema.py
@@ -4,7 +4,11 @@ DEBUG = False
 
 COMMANDS = [
     "car",
-    "cdr"
+    "cdr",
+    "cons",
+    "null?",
+    "atom?",
+    "eq?"
 ]    
 
 def debug(*args):
@@ -21,6 +25,31 @@ def check_parenthesis(string):
         if open < 0:
             return False
     return open == 0
+
+
+def tokenize_parameters(string):
+    parameters = []
+    depth = 0
+    start_index = 0
+
+    for i, char in enumerate(string):
+        if char == '(':
+            depth += 1
+        elif char == ')':
+            depth -= 1
+        elif char == ' ' and depth == 0:
+            parameter = string[start_index:i].strip()
+            if parameter:
+                parameters.append(tokenize(parameter))
+            start_index = i + 1
+
+    # Add last parameter
+    last_parameter = string[start_index:].strip()
+    
+    if last_parameter:
+        parameters.append(tokenize(last_parameter))
+
+    return parameters
 
 def tokenize(string):
     debug(f"Tokenizing: {string}")
@@ -42,12 +71,40 @@ def tokenize(string):
     first_element = string.split(" ")[0]
     if first_element in COMMANDS:
         debug(f"Command: {string}")
-        if first_element == "car":
-            return CAR(tokenize(" ".join(string.split(" ")[1:])))
-        elif first_element == "cdr":
-            return CDR(tokenize(" ".join(string.split(" ")[1:])))
+        if first_element in COMMANDS:
+            parameters = tokenize_parameters(" ".join(string.split(" ")[1:]))
+            if first_element == "car":
+                if len(parameters) != 1:
+                    print("Error: car: takes only one argument")
+                    exit(1)
+                return CAR(parameters[0])
+            elif first_element == "cdr":
+                if len(parameters) != 1:
+                    print("Error: cdr: takes only one argument")
+                    exit(1)
+                return CDR(parameters[0])
+            elif first_element == "cons":
+                if len(parameters) != 2:
+                    print("Error: cdr: takes two arguments")
+                    exit(1)
+                return CONS(parameters[0], parameters[1])
+            elif first_element == "null?":
+                if len(parameters) != 1:
+                    print("Error: null?: takes only one argument")
+                    exit(1)
+                return NULL(parameters[0])
+            elif first_element == "atom?":
+                if len(parameters) != 1:
+                    print("Error: atom?: takes only one argument")
+                    exit(1)
+                return IS_ATOM(parameters[0])
+            elif first_element == "eq?":
+                if len(parameters) != 2:
+                    print("Error: eq?: takes two arguments")
+                    exit(1)
+                return EQ(parameters[0], parameters[1])
            
-    result_lst = S_List()
+    result_lst = S_List([])
     s_exp = ""
     i = 0
     
@@ -57,7 +114,7 @@ def tokenize(string):
             # only if the string is not empty, tokenize it
             if len(s_exp) > 0:
                 debug(f"Tokenizing from space: {s_exp}")
-                result_lst.append(tokenize(s_exp))
+                result_lst.items.append(tokenize(s_exp))
             s_exp = ""
             i += 1
         # it's an other list to parse, need to find the end of the list
@@ -74,7 +131,7 @@ def tokenize(string):
                 s_exp += string[i]
                 i += 1
             debug(f"tokenizing from p: {s_exp}")
-            result_lst.append(tokenize(s_exp))
+            result_lst.items.append(tokenize(s_exp))
             s_exp = ""
         else:
             # evaluating an atom
@@ -83,16 +140,17 @@ def tokenize(string):
 
     if len(s_exp) > 0:
         debug(f"tokenizing from end: {s_exp}")
-        result_lst.append(tokenize(s_exp))
+        result_lst.items.append(tokenize(s_exp))
 
     return result_lst
     
 if __name__ == '__main__':
-    value = "(car (cdr (cdr (a b c))))"
+    value = "(eq? (car (a a b)) (car (cdr (a a b))))"
     
     if not check_parenthesis(value):
         print("Error: Invalid S-Expression")
         exit(1)
 
     print(value)
+    print(tokenize(value))
     print(tokenize(value).execute())
